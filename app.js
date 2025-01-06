195a970bdbf4bdc8766b2d4e4c6f5acc4cb66d1d
Muhammad Noman <122905854+muhammad-nomanali@users.noreply.github.com>
2025-01-06 09:29:39 -0800

    ee

--- app.js
+++ app.js
@@ -47,3 +47,28 @@ moveButton.addEventListener('click', () => {
     }
 
 
+    // 
+    function toggleDropdowns() {
+      const dropdown = document.getElementById("myDropdowns");
+      // Toggle the display of the dropdown
+      if (dropdown.style.display === "block") {
+          dropdown.style.display = "none";
+      } else {
+          dropdown.style.display = "block";
+      }
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+      if (!event.target.matches('.telegram-icon') && !event.target.matches('.dropdown-contents')) {
+          const dropdowns = document.getElementsByClassName("dropdown-contents");
+          for (let i = 0; i < dropdowns.length; i++) {
+              const openDropdown = dropdowns[i];
+              if (openDropdown.style.display === "block") {
+                  openDropdown.style.display = "none";
+              }
+          }
+      }
+  }
+
+
